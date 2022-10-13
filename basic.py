c74299e73706b31ff9bdafda6d17febc6e23b31e
habib <hab67603@gmail.com>
2022-10-13 15:18:16 +0500

    add new data

---
+++ basic.py
@@ -0,0 +1,13 @@
+"""def printname(fname,mname,lname):
+  printname(fname="Habib",mname="Ur",lname="Rahman")
+  print("your name is",[1])"""
+  
+"""def add():
+  a=input("Enter first number")
+  b=input("Enter second number")
+  add=int(a)+int(b)
+  print(add)"""
+  
+def add(a,b):
+  c=a+b
+  print("the sum is",c) 
