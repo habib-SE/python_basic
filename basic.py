b7e0662ff1b3622a078a4ec559af5c7980122286
habib <hab67603@gmail.com>
2022-10-16 00:40:01 +0500

    add new data

--- basic.py
+++ basic.py
@@ -1,13 +1,26 @@
-"""def printname(fname,mname,lname):
-  printname(fname="Habib",mname="Ur",lname="Rahman")
-  print("your name is",[1])"""
-  
-"""def add():
-  a=input("Enter first number")
-  b=input("Enter second number")
-  add=int(a)+int(b)
-  print(add)"""
-  
-def add(a,b):
-  c=a+b
-  print("the sum is",c) 
+# def printname(name):
+#   print("your name is ",name[4])
+# printname("habib")
+
+def sub(a, b):
+    c = abs(a-b)
+    print("the sub is", c)
+
+
+def add(a, b):
+    c = a+b
+    print("the sum is", c)
+    num1 = int(input("Enter first number"))
+    num2 = int(input("Enter second number"))
+    print("1.....addition")
+    print("2.....subtraction")
+    choice = int(input("Enter your choice"))
+    if choice == 1:
+        add(num1, num2)
+    else:
+        sub(num1, num2)
+
+
+# from datetime import datetime
+# habib=datetime.now()
+# print("This s current date and time",habib.strftime("%d/%m/%Y %I:%M %A"))
